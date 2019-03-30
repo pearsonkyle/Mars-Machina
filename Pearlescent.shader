75bbe85ee764f2fc0c21b85ac2ed890ac1bb0058
Kyle A. Pearson <pearsonk@email.arizona.edu>
2019-03-29 20:31:54 -0700

    Add files via upload

---
+++ Pearlescent.shader
@@ -0,0 +1,100 @@
+﻿// Upgrade NOTE: upgraded instancing buffer 'Props' to new syntax.
+
+Shader "Custom/Pearlescent" {
+	Properties {
+		_Color ("Color", Color) = (1,1,1,1)
+		_ColorDirect1("Indirect Color 1", Color) = (1,1,1,1)
+		_ColorDirect2("Indirect Color 2", Color) = (1,1,1,1)
+		_ColorMix("Color Tolerance", Range(0,1)) = 0.5
+		//_MainTex ("Albedo (RGB)", 2D) = "white" {}
+		_Glossiness ("Smoothness", Range(0,1)) = 0.5
+		_Metallic ("Metallic", Range(0,1)) = 0.0
+		_Iterations("Iridescence Function Iterations (Power)", Range(0,20)) = 5
+	}
+	SubShader {
+		Tags { "RenderType"="Opaque" }
+		LOD 200
+		
+		CGPROGRAM
+		// Physically based Standard lighting model, and enable shadows on all light types
+		#pragma surface surf Standard fullforwardshadows
+
+		// Use shader model 3.0 target, to get nicer looking lighting
+		#pragma target 3.0
+
+		sampler2D _MainTex;
+
+		struct Input {
+			float2 uv_MainTex;
+			float3 viewDir;
+		};
+
+		half _Glossiness;
+		half _Metallic;
+		fixed4 _Color;
+		fixed4 _ColorDirect1;
+		fixed4 _ColorDirect2;
+
+		float _ColorMix;
+		float _Iterations;
+
+		// Add instancing support for this shader. You need to check 'Enable Instancing' on materials that use the shader.
+		// See https://docs.unity3d.com/Manual/GPUInstancing.html for more information about instancing.
+		// #pragma instancing_options assumeuniformscaling
+		UNITY_INSTANCING_BUFFER_START(Props)
+			// put more per-instance properties here
+		UNITY_INSTANCING_BUFFER_END(Props)
+
+		float map(float val, float min, float max, float newMin, float newMax)
+		{
+			float ratio = abs(val - min) / abs(max - min);
+			return newMin + ratio * abs(newMax - newMin);
+		}
+
+		float4 mix(float4 col1, float4 col2, float4 col3, float mixAmount)
+		{
+			//mixAmount = abs(mixAmount);
+			
+			if (mixAmount < _ColorMix){
+				return float4(col1.r * mixAmount + col2.r * (1 - mixAmount), col1.g * mixAmount + col2.g * (1 - mixAmount), col1.b * mixAmount + col2.b * (1 - mixAmount), col1.a * mixAmount + col2.a * (1 - mixAmount));				
+			}
+			else //if (mixAmount >= 0.5)
+			{
+				return float4(col2.r * mixAmount + col3.r * (1 - mixAmount), col2.g * mixAmount + col3.g * (1 - mixAmount), col2.b * mixAmount + col3.b * (1 - mixAmount), col2.a * mixAmount + col3.a * (1 - mixAmount));
+			}
+			
+		}
+
+		float efficientIridFunc(float x) {
+			return x*x*x*x*x;
+		}
+		
+		float iridFunc(float x)
+		{
+			if (x < 0) x = 0; //get rid of the black outline caused by a thin layer of almost-perpendicular pixels
+			float result = pow(x, _Iterations);
+			return result;
+		}
+
+		void surf (Input IN, inout SurfaceOutputStandard o) {
+			
+			float3 worldNormal = WorldNormalVector(IN, o.Normal);
+
+			float dotProduct = dot(worldNormal, IN.viewDir); //will always be between 1 and 0 for the pixels that we care about because normals facing away will be 0 to -1. I hope I don't have these numbers the wrong way around...
+			//float rampPos = map(dotProduct, -1, 1, 0, 1);
+
+			float4 col;
+
+			//col = mix(_Color, _ColorDirect, efficientIridFunc(dotProduct));
+			col = mix(_Color, _ColorDirect1, _ColorDirect2, iridFunc(dotProduct));
+
+			o.Albedo = col;
+			// Metallic and smoothness come from slider variables
+			o.Metallic = _Metallic;
+			o.Smoothness = _Glossiness;
+			//o.Alpha = c.a;
+		}
+		ENDCG
+	}
+	FallBack "Diffuse"
+}
---
+++ meshGenerator.cs
@@ -0,0 +1,148 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using TensorFlow;
+public class meshGenerator : MonoBehaviour
+{
+    // stuff for creating a simple mesh
+    Material material;
+    Mesh mesh;
+    Vector3[] vertices;
+    int[] triangles;
+    Vector2[] uvs;
+
+    // output size from NN float[784] -> float[28,28]
+    public int xSize = 96;
+    public int ySize = 96;
+
+
+    // input to machine learning model 
+    public TextAsset graphModel;
+    TFGraph graph;
+    TFSession session;
+
+    private float[,] inputData =
+        new float[1, 2];
+    public float latent_x;
+    public float latent_y;
+    public float latent_speed = 1f;
+
+    void Start()
+    {
+        
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+        material = GetComponent<Renderer>().material;
+
+        // Create the TensorFlow model
+        graph = new TFGraph();
+        graph.Import(graphModel.bytes);
+        session = new TFSession(graph);
+
+        // start generating at a random point in latent parameter space
+        latent_x = Random.value;
+        latent_y = Random.value;
+    }
+
+
+    void CreateShape()
+    {
+        // Run the model
+        var runner = session.GetRunner();
+        runner.AddInput(graph["decoder_input"][0], inputData);
+        runner.Fetch(graph["decoder_output/Sigmoid"][0]);
+        float[,,,] newImage = runner.Run()[0].GetValue() as float[,,,];
+
+        // Debug.Log("new image:"+newImage.Length);
+
+        vertices = new Vector3[(xSize + 1) * (ySize + 1)];
+
+        int i = 0;
+        for (int y = 0; y < ySize + 1; y++)
+        {
+            for (int x = 0; x < xSize + 1; x++)
+            {
+                vertices[i] = new Vector3( (float)x/xSize, (float)y/ySize, newImage[0, Mathf.Clamp(x, 0, xSize-1), Mathf.Clamp(y,0,ySize-1), 0] );
+
+                i++;
+            }
+        }
+
+        triangles = new int[xSize * ySize * 6];
+
+        int vert = 0;
+        int tris = 0;
+        for (int y = 0; y < ySize; y++)
+        {
+            for (int x = 0; x < xSize; x++)
+            {
+
+                triangles[tris + 0] = vert + 0;
+                triangles[tris + 1] = vert + xSize + 1;
+                triangles[tris + 2] = vert + 1;
+                triangles[tris + 3] = vert + 1;
+                triangles[tris + 4] = vert + xSize + 1;
+                triangles[tris + 5] = vert + xSize + 2;
+
+                vert++;
+                tris += 6;
+            }
+            vert++;
+        }
+    }
+
+    
+    void UpdateMesh()
+    {
+        mesh.Clear();
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.RecalculateNormals();
+    }
+    private void OnDrawGizmos()
+    {
+        if (vertices == null)
+            return;
+
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            Gizmos.DrawSphere(vertices[i], 0.1f);
+        }
+    }
+
+
+    Touch touch;
+    float lastTime=0;
+    // Update is called once per frame
+    void Update()
+    {
+        CreateShape();
+        UpdateMesh();
+
+        // touch controls
+        if (Input.touchCount == 1)
+        {
+            // rotate player based on position derivative
+            if (Input.GetTouch(0).phase == TouchPhase.Moved)
+            {
+                touch = Input.GetTouch(0);
+                latent_x = 4*(touch.position.x - Screen.width/2)/Screen.width;
+                latent_y = 4*(touch.position.y - Screen.height/2) / Screen.height;
+
+                inputData[0, 0] = latent_x;
+                inputData[0, 1] = latent_y;
+                lastTime = Time.time;
+            }
+        }
+        else
+        {
+            // input data should only vary between ~-4 -> 4
+            inputData[0, 0] = latent_x + 2 * Mathf.Sin(latent_speed* 0.2f * (Time.time-lastTime));
+            inputData[0, 1] = latent_y + 2 * Mathf.Sin(1.5f + 0.25f * latent_speed * (Time.time-lastTime));
+        }
+        // Color of texture will change depending on ange of reflected light
+        //Color newColor = new Vector4(inputData[0,0], inputData[0, 1]*0.75f, inputData[0, 1], 1);
+        //material.SetColor("_ColorDirect1", newColor);
+    }
+}
